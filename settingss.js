df1495c92580e635756f3d110021605faf4cb8ff
Cavy-cpu <agyemanggodwin77@gmail.com>
2025-08-02 14:34:20 -0700

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~F7UBGJgY#j8N0OL1cyY5MzmmuVfYLmTGFvZ1WhLYQhFWa_3UDIo ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "233538149721", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
